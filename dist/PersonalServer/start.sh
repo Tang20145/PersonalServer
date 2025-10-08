80a5ebe9417fbf74ac46450ee53533339c76b867
JcGam1ng <2323024922@qq.com>
2025-10-08 11:09:35 +0800

    成功运行

--- dist/PersonalServer/start.sh
+++ dist/PersonalServer/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 注意区分() 与 {}
-export LD_LIBRARY_PATH=$(pwd)/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:../../extern/mysql-connector-c++/lib64/
 
 echo "ld path : ${LD_LIBRARY_PATH}"
 
 # 需要 sudo ./start.sh 执行，因为绑定的是80端口，需要root权限
-sudo nohup ./PersonalServer > output.log 2>&1 &
+nohup ./PersonalServer > output.log 2>&1 &
 
 echo "start finish"
